5f109af67c29cb2fa393ed9b9172041c2cbc11de
bbqnow <bbqfast@hotmail.com>
2013-01-03 16:23:35 -0800

    local
    local

---
+++ src/iviewj/TestBase.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package iviewj;
+
+/**
+ *
+ * @author alan.chung
+ */
+public class TestBase {
+    protected boolean debug = false;
+    public void w(String s)
+    {
+        System.out.println(s);
+    }
+
+    
+
+}
